e3b91830fe4ec095dd377b5df603c5336ca17e89
21bmiit110 <21bmiit110@gmail.com>
2025-09-28 19:24:43 +0000

    mini2

--- src/lib/movies.ts
+++ src/lib/movies.ts
@@ -48,7 +48,7 @@ export const movies: Media[] = [
       {
         season: 1,
         episodes: [
-          { episode: 1, title: 'Episode 1', description: 'Arisu and his friends run from the police, only to find themselves in a deserted Tokyo. They are forced to play a deadly game to survive.', videoUrl: '/video/Alice_in_Borderland/s1/E1.mp4' },
+          { episode: 1, title: 'Episode 1', description: 'Arisu and his friends run from the police, only to find themselves in a deserted Tokyo. They are forced to play a deadly game to survive.', videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
           { episode: 2, title: 'Episode 2', description: 'The game of tag continues. Arisu must use his wits to save his friends and a mysterious woman named Usagi.', videoUrl: '/video/Alice_in_Borderland/s1/E2.mp4' },
         ]
       },
